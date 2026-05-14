702a9b748d6f296e33e6f30c949de97048ab4643
HYXK-maker <2539854256@qq.com>
2026-05-14 17:45:23 +0800

    feat:导入暗黑样式
    在 main.js 中导入暗黑样式

--- dev1/src/Web/knowledge-base-frontend/src/main.js
+++ dev1/src/Web/knowledge-base-frontend/src/main.js
@@ -5,6 +5,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 const app = createApp(App)
 
